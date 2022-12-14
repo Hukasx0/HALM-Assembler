192682e089943f8c2eaf0d1e2f8e1de73f3b8d42
Hukasx0 <hubertkasp13@gmail.com>
2022-12-14 20:07:55 +0100

    Added mov and int parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
 module Main where
 
+import System.Environment
+import Data.List
+import Text.Parsec
+import Text.Parsec.String
+
+
+type Register = String
+data Operation = Mov Register Register | Interrupt String
+                deriving(Eq,Show)
+
+letterDigitParser :: Parsec String () Char
+letterDigitParser = oneOf ['0'..'9'] <|> oneOf ['a'..'z'] <|> oneOf ['A'..'Z']
+
+movParser :: Parser Operation
+movParser = Mov <$>  (string "mov" >> many1 space >> many1 letter) <*> (char ',' >> many1 space >> many1 letterDigitParser)
+
+interruptParser :: Parser Operation
+interruptParser = Interrupt <$> (string "int" >> many1 space >> many1 letterDigitParser)
+
+finalParser :: Parser Operation
+finalParser = try movParser <|> interruptParser
+
+virtualFile :: String
+virtualFile = "mov al, 123\n mov bl, 0xA\n mov dl, al\n int 0x10 "
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print $ parse (spaces >> many (finalParser <* many1 space) <* eof) "virtual" virtualFile
