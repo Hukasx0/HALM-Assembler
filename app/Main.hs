c1d15e713447e4f86884d46ea2c0f4d2a77c14d8
Hukasx0 <hubertkasp13@gmail.com>
2022-12-16 18:46:33 +0100

    Added register, int and hex parsing

--- app/Main.hs
+++ app/Main.hs
@@ -6,45 +6,73 @@ import Text.Parsec
 import Text.Parsec.String
 
 
-type Register = String
 type Label = String
-type RegOrVal = String
 
-data Operation = Mov Register RegOrVal | Interrupt String | Inc Register | Dec Register | Cmp Register RegOrVal | Jmp Label
+data Value = Register String | Int String | Hex String
+                deriving(Eq,Show)
+
+data Operation = Mov Value Value | Interrupt Value | Inc Value | Dec Value | Cmp Value Value | Jmp Label
                 deriving(Eq,Show)
 
 letterDigitParser :: Parsec String () Char
 letterDigitParser = oneOf ['0'..'9'] <|> oneOf ['a'..'z'] <|> oneOf ['A'..'Z']
 
+-- value parsers
+
+registerParser :: Parser Value
+registerParser = Register <$> (string "al" <|> string "bl" <|> string "cl" <|> string "dl")
+
+intParser :: Parser Value
+intParser = Int <$> (many1 digit)
+
+hexParser :: Parser Value
+hexParser = Hex <$> (string "0x" >> many1 hexDigit)
+
+anyValParser :: Parser Value
+anyValParser = try registerParser <|> try hexParser <|> intParser
+
+onlyValParser :: Parser Value
+onlyValParser = try hexParser <|> intParser
+
+valToBin :: Value -> String
+valToBin(Register a) = "(register "++a++")"
+valToBin(Int b) = "(Integer value "++b++")"
+valToBin(Hex c) = "(Hexadecimal value 0x"++c++")"
+
+--
+
+-- instruction parsers
+
 movParser :: Parser Operation
-movParser = Mov <$>  (string "mov" >> many1 space >> many1 letter) <*> (char ',' >> spaces >> many1 letterDigitParser)
+movParser = Mov <$>  (string "mov" >> many1 space >> registerParser) <*> (char ',' >> spaces >> anyValParser)
 
 interruptParser :: Parser Operation
-interruptParser = Interrupt <$> (string "int" >> many1 space >> many1 letterDigitParser)
+interruptParser = Interrupt <$> (string "int" >> many1 space >> onlyValParser)
 
 incParser :: Parser Operation
-incParser = Inc <$> (string "inc" >> many1 space >> many1 letterDigitParser)
+incParser = Inc <$> (string "inc" >> many1 space >> registerParser)
 
 decParser :: Parser Operation
-decParser = Dec <$> (string "dec" >> many1 space >> many1 letterDigitParser)
+decParser = Dec <$> (string "dec" >> many1 space >> registerParser)
 
 jmpParser :: Parser Operation
 jmpParser = Jmp <$> (string "jmp" >> many1 space >> many1 letterDigitParser)
 
 cmpParser :: Parser Operation
-cmpParser = Cmp <$>  (string "cmp" >> many1 space >> many1 letter) <*> (char ',' >> spaces >> many1 letterDigitParser)
+cmpParser = Cmp <$>  (string "cmp" >> many1 space >> anyValParser) <*> (char ',' >> spaces >> anyValParser)
 
 finalParser :: Parser Operation
 finalParser = try movParser <|> try interruptParser <|> try incParser <|> try decParser <|> try cmpParser <|> try jmpParser
 
 insToBin :: Operation -> String
-insToBin (Mov a b) = "copying value from "++b++" to "++a
-insToBin (Interrupt code) = "interrupting with code "++code
-insToBin (Inc reg) = "incrementing "++reg++" value"
-insToBin (Dec reg) = "decrementing "++reg++" value"
-insToBin (Cmp a b) = "comparing "++a++" with "++b
+insToBin (Mov a b) = "copying value from "++(valToBin $ b)++" to "++(valToBin $ a)
+insToBin (Interrupt code) = "interrupting with code "++(valToBin $ code)
+insToBin (Inc reg) = "incrementing "++(valToBin $ reg)++" value"
+insToBin (Dec reg) = "decrementing "++(valToBin $ reg)++" value"
+insToBin (Cmp a b) = "comparing "++(valToBin $ a)++" with "++(valToBin $ b)
 insToBin (Jmp l) = "unconditional jump to "++l
 
+--
 codeToIns :: [Operation] -> String
 codeToIns code = unlines $ map insToBin code
 
