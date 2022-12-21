39759c1a552cc75fe8c6e5956a546d80c289e4b4
Hukasx0 <hubertkasp13@gmail.com>
2022-12-21 18:22:00 +0100

    Added parsing from file

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import Text.Parsec
 import Data.Word
+import System.Environment
 import qualified Data.ByteString.Lazy as B
 
 import Values
@@ -11,13 +12,16 @@ codeToIns :: [Operation] -> [[Word8]]
 codeToIns code = map insToBin code
 
 main :: IO ()
-main = do putStrLn $ ("input:\n"++virtualFile) 
-          let parsed = parse (spaces >> many (finalParser <* many1 space) <* eof) "virtual" virtualFile
+main = do
+          fileName <- head <$> getArgs
+          fContent <- readFile $ fileName
+          putStrLn $ ("input:\n"++fContent) 
+          let parsed = parse (spaces >> many (finalParser <* many1 space) <* eof) fileName fContent
           putStrLn $ "output:"
           case parsed of
             Left err -> print err
             Right corr -> do
-                          putStrLn "Writing binary to out.bin"
-                          B.writeFile "out.bin" (B.pack $ concat $ codeToIns $ corr)
+                          putStrLn ("Writing binary to "++fileName++".bin")
+                          B.writeFile (fileName++".bin") (B.pack $ concat $ codeToIns $ corr)
                           print (concat $ codeToIns $ corr)
  
